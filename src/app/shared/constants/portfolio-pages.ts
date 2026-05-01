82d01e8e3968558c26100a0e65cb4cff913c6dcd
Коломыцев Андрей <akolomytsev@softrust.ru>
2026-05-01 19:52:02 +0300

    Поправлен header

--- src/app/shared/constants/portfolio-pages.ts
+++ src/app/shared/constants/portfolio-pages.ts
@@ -2,5 +2,5 @@ import {PortfolioPage} from "../interfaces/portfolio-page.interface";
 import {PortfolioCategory} from "../enums/portfolio-category.enum";
 
 export const PORTFOLIO_PAGES: PortfolioPage[] = [
-  { slug: PortfolioCategory.InteriorPainting, title: 'Роспись интерьера', route: '/portfolio/interior-painting' },
+  { slug: PortfolioCategory.InteriorPainting, title: 'Роспись интерьера' },
 ];
--- src/app/shared/interfaces/portfolio-page.interface.ts
+++ src/app/shared/interfaces/portfolio-page.interface.ts
@@ -3,5 +3,4 @@ import {PortfolioCategory} from "../enums/portfolio-category.enum";
 export interface PortfolioPage {
   slug: PortfolioCategory;
   title: string;
-  route: string;
 }
