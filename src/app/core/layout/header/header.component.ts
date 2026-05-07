ad49e37f794dd332d1aa1a3ee6b7746d48c93c1a
Коломыцев Андрей <akolomytsev@softrust.ru>
2026-05-08 00:02:30 +0300

    Добавляем кнопочку закрытия

--- src/app/core/layout/header/header.component.ts
+++ src/app/core/layout/header/header.component.ts
@@ -1,4 +1,4 @@
-import {ChangeDetectionStrategy, Component, signal} from '@angular/core';
+import {ChangeDetectionStrategy, Component, effect, signal} from '@angular/core';
 import { RouterLink } from '@angular/router';
 import {PORTFOLIO_CATEGORIES} from "../../../shared/constants/portfolio-categories";
 import {PortfolioCategory} from "../../../shared/interfaces/portfolio-category.interface";
@@ -15,6 +15,14 @@ export class HeaderComponent {
   protected categories: PortfolioCategory[] = PORTFOLIO_CATEGORIES;
 
   readonly isMenuOpen = signal(false);
+
+  constructor() {
+    effect(() => {
+      const open = this.isMenuOpen();
+
+      document.body.style.overflow = open ? 'hidden' : '';
+    });
+  }
 
   toggleMenu(): void {
     this.isMenuOpen.update(v => !v);
